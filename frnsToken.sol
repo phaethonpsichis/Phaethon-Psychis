f5ea5535d1eac3777eefbf63140a39f354580d79
Phaethon Psychis <80890815+rndfactory@users.noreply.github.com>
2022-02-02 11:50:35 +0200

    Update frnsToken.sol

--- frnsToken.sol
+++ frnsToken.sol
@@ -1,3 +1,14 @@
-//ERC20 token 
-//Introduce a dynamic distribution "pull" method 
-//Access control mechanism based on token Id
+// @title  
+// 1. A new smart contract to create ERC20 tokens
+// 2. A distribution plan structure to associate ERC1155 token-ID to ERC20 token amount
+// 2.1 Introducing a Struct {
+    ERC1155 token ID
+    Contract address
+// } 
+// 2.2 mapping (Struct []=> amount) 
+// 3. A storage structure to store information about claimed tokens
+//  mapping ((Struct []=> amount) => bool) // all values by default set to false
+// 
+// 4. A distribution function to allow eligible owners to claim their tokens
+// 4.1 When tokens claimed => update mapping
+// 
